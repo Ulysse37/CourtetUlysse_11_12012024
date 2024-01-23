1a764cea6e610936f760844ec78cc0442206ce86
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-01-23 10:35:24 +0100

    style banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
   return (  
     <div className="banner-container">
       <img className="banner-img" src={homeBanner} alt='Bannière kasa' />
-      <h1 className="banner-text">Chez vous, partout et ailleurs</h1>
+      <h2 className="banner-text">Chez vous, partout et ailleurs</h2>
     </div>
   )
 }

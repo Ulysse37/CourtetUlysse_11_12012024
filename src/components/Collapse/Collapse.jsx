9cd374a337ec4f1229ba44cffdfeb1ca814a5bef
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-01-25 23:39:25 +0100

    Style + effet collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,8 +1,20 @@
 import './collapse.css';
+import { useState } from 'react';
 import collapseData from '../../assets/about.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 function Collapse() {
+  const [selected, setSelected] = useState(null)
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null)
+    }
+
+    setSelected(i)
+  }
+
   return (
     <section className='wrapper'>
       <div className='accordion'>
@@ -10,11 +22,11 @@ function Collapse() {
           <div className='item'>
             <div className='collapse-title'>
               <h3>{item.title}</h3>
-              <span>
-                <FontAwesomeIcon icon={faChevronUp} />
+              <span onClick={() => toggle(i)}>
+                {selected === i ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
               </span>
             </div>
-            <div className='collapse-text'>
+            <div className={selected === i ? 'collapse-text show' : 'collapse-text' }>
               <p>{item.text}</p>
             </div>
           </div>

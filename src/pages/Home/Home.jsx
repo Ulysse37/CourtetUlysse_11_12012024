9c80fdb6b4c657a426fddc082ed57335d3b1eb1b
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-01-26 14:20:44 +0100

    ajout font pour la page error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import Banner from '../../components/Banner/Banner';
 import homeBanner from '../../assets/IMG.png';
+import data from '../../assets/logements.json';
 
 function Home() {
+  console.log(data);
   return (
     <main>
       <Banner imageSrc={homeBanner} imageAlt="Falaise au dessus de l'océan" text="Chez vous, partout et ailleurs" />

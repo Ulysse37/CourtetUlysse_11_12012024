47b7d2defbcf1bb18dfe4c2cf418374f19a67b9b
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-01-26 15:21:21 +0100

    correction style détails compte rendu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 function NavBar() {
+  const { pathname } = useLocation();
+
   return (
     <nav>
       <img className='navbar-logo' src={logo} alt='logo Kasa' />
       <ul>
         <li>
-          <Link to="/">Accueil</Link>
+          <Link to="/" className={`${pathname === "/" ? "active" : ""} accueil-link`}>Accueil</Link>
         </li>
         <li>
-          <Link to="/About">A propos</Link>
+          <Link to="/About" className={pathname === "/About" ? "active" : ""}>A propos</Link>
         </li>
       </ul>
     </nav>

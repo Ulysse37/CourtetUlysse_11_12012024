728ca5a599717e55b75fbbd252c2fc0d8621c9fa
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-01-30 17:51:31 +0100

    ajout component Card  et la boucle sur la page home

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './card.css';
+
+function Card({ id, title, cover, onClick }) {
+
+  return (
+    <figure className='card' onClick={onClick}>
+      <img src={cover} alt={title}></img>
+      <Link to={`/Accomodation/${id}`}>
+        <figcaption>{title}</figcaption>
+      </Link>
+    </figure>
+  )
+}
+
+export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Banner from '../../components/Banner/Banner';
 import homeBanner from '../../assets/IMG.png';
+import Card from '../../components/Card/Card'
 import data from '../../assets/logements.json';
 
 function Home() {
-  console.log(data);
+  //console.log(data);
   return (
     <main>
       <Banner imageSrc={homeBanner} imageAlt="Falaise au dessus de l'océan" text="Chez vous, partout et ailleurs" />
+      <div className='card-container'>
+        {data.map((accomodation) => (
+          <Card 
+            key={accomodation.id}
+            id={accomodation.id}
+            title={accomodation.title}
+            cover={accomodation.cover}
+            /* onClick={() => handleCardClick(accomodation.id)} */
+           /> 
+        ))}
+      </div>
     </main>
   );
 }

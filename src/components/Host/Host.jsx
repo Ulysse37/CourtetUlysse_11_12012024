3008ff7de74f2ea4e6c6d433a3096d4745117fb0
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-02-01 12:37:03 +0100

    ajout rating page host + style rating + host / en mobile

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -1,11 +1,13 @@
 import './host.css';
 
 function Host( { name, picture } ) {
+  // sépare nom et prénom de l'hôte pour faire saut de ligne entre les 2
+  const [firstName, lastName] = name.split(' ');
 
   return (
-    <aside>
-      <p>{name}</p>
-      <img src={picture} alt={"photo de " + name}></img>
+    <aside className='host-info-container'>
+      <p className='host-name'>{firstName} <br /> {lastName}</p>
+      <img className='host-img' src={picture} alt={"photo de " + name}></img>
     </aside>
   )
 }
--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -4,12 +4,14 @@ import data from '../../assets/logements.json';
 import Carousel from '../../components/Carousel/Carousel'
 import Tag from '../../components/Tag/Tag';
 import Host from '../../components/Host/Host';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function Accomodation() {
   const { id } = useParams();
   
-  const accomodation = data.find(item => item.id === id); // trouve le logement correspondant à l'id 
-  
+  const accomodation = data.find(item => item.id === id); // trouve le logement correspondant à l'id de la page
+
   return (
     <main>
       <Carousel 
@@ -26,7 +28,16 @@ function Accomodation() {
         ))}
         </ul>
       </aside>
-      <article>
+      <article className='host-container'>
+        <aside className="rating">
+          {[...Array(5)].map((_, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              className={index < accomodation.rating ? "full-star" : "empty-star"}
+            />
+          ))}
+        </aside>
         <Host 
           name={accomodation.host.name}
           picture={accomodation.host.picture}/>

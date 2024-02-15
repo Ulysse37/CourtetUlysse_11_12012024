d5f1beae83efbf70fea4572ae14ab83458f4e7e8
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-02-15 09:11:18 +0100

    ajout linear gradient card composant

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,9 @@ function Card({ id, title, cover }) {
     <Link to={`/Accomodation/${id}`} className='card'>
       <figure className='card-figure'>
         <img src={cover} alt={title} className='card-img'></img>
-        <figcaption className='card-figcaption'>{title}</figcaption>
+        <div className='card-linear-gradient'>
+          <figcaption className='card-figcaption'>{title}</figcaption>
+        </div>
       </figure>
     </Link> 
   )
